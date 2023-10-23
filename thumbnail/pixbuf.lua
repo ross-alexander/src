9b2abacdef82e8ac5c4c590b9d3cff664366c2a4
Ross Alexander <evilross@yahoo.co.uk>
2023-10-23 13:55:25 +0100

    Update comments.

--- thumbnail/pixbuf.lua
+++ thumbnail/pixbuf.lua
@@ -1,20 +1,42 @@
---local pixbuf = image_t.new_pixbuf_from_file("286.jpg")
---print(pixbuf)
---local scaled = pixbuf:scale_to(400)
---scaled:save("pixbuf.jpeg")
---local blank = image_t.new_pixbuf(2000, 2000)
---print(blank)
---blank:fill("orange")
---blank:compose(scaled, 100, 100)
---blank:save("blank.jpeg")
+-- ----------------------------------------------------------------------
+--
+-- 2023-10-20: Add comments
+--
+-- ----------------------------------------------------------------------
+
+function example()
+   local pixbuf = image_t.new_pixbuf_from_file("images/286.jpg")
+   print(pixbuf)
+   local scaled = pixbuf:scale_to(400)
+   scaled:save("pixbuf.jpeg")
+   
+   local blank = image_t.new_pixbuf(2000, 2000)
+   print(blank)
+   blank:fill("orange")
+   blank:compose(scaled, 100, 100)
+   blank:save("blank.jpeg")
+end
+
+-- ----------------------------------------------------------------------
+--
+-- compoase
+--
+-- ----------------------------------------------------------------------
 
 function compose(index, tn, cols, size, border)
+   -- Load the images into memory
+
    tn:load_images_pixbuf()
+
+   -- Calculate number of rows
+
    local images = tn.image_table
    local rows = ((#images-1)//cols)+1
    local fontsize = 16
    print("rows ", rows)
 
+   -- Create result pixbuf
+   
    local width = (size+border)*cols + border
    local height = (size+border+fontsize)*rows + border
    local dst = image_t.new_pixbuf(width, height)
@@ -55,11 +77,31 @@ function compose(index, tn, cols, size, border)
    dst:save(string.format("pixbuf-%02d.jpg", index))
 end   
 
+function thumbnail()
+   local tn = thumbnail_t.new()
 
-local tn = thumbnail_t.new()
-tn:dir_add("/locker/images/others")
-tn:dir_scan()
-local split = tn:split(32)
-for i,v in ipairs(split) do
-   compose(i, v, 8, 200, 5)
+   -- Add local directory "test"
+   
+   tn:dir_add("test")
+   
+   -- Run recursive scan on directory
+
+   tn:dir_scan()
+
+   -- Split into multiple thumbnail_t objects with a maximum of 32
+   -- images each
+   
+   local split = tn:split(32)
+
+   -- Loop over table (indexed 1 .. n)
+
+   for i,v in ipairs(split) do
+      -- Compose 8 images per row, 200 pixels image size, bordre 5 pixels wide
+      compose(i, v, 8, 200, 5)
+   end
 end
+
+
+-- example()
+thumbnail()
+
