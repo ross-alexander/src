1bff909d3816313607d5d8e1042981cf4679fe6d
Ross Alexander <evilross@yahoo.co.uk>
2023-11-08 19:58:04 +0000

    Add JS files

---
+++ biohazard/bio.js
@@ -0,0 +1,19 @@
+{
+    "line_width":2.0,
+    "inner_radius":60.1,
+    "inner_offset":90,
+    "outer_radius":80,
+    "outer_offset":70,
+    "center_radius":18,
+    "center_offset":5,
+    "ring_outer": 75,
+    "ring_inner": 55,
+    "ring_offset":8,
+    "scale":2.6,
+    "width":800,
+    "height":800,
+    "fill":"#FF0000FF",
+    "stroke":"#000000FF",
+    "constuction": false,
+    "debug": false
+}
---
+++ biohazard/defaults.js
@@ -0,0 +1,18 @@
+{
+    "line_width":2.0,
+    "inner_radius":11.5,
+    "inner_offset":17.6,
+    "outer_radius":14.0,
+    "outer_offset":15.0,
+    "center_radius":4.0,
+    "center_offset":1.0,
+    "ring_outer": 15.0,
+    "ring_inner": 11.0,
+    "ring_offset":1.0,
+    "scale":13.0,
+    "width":800,
+    "height":800,
+    "fill":"#ffff00FF",
+    "stroke":"#000000FF",
+    "constuction": true
+}
