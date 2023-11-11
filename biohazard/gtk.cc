ab78c583dcd98e8665d623d7f6ac6d75a5b68dac
Ross Alexander <evilross@yahoo.co.uk>
2023-11-11 13:32:17 +0000

    Convert to stylesheets.

--- biohazard/gtk.cc
+++ biohazard/gtk.cc
@@ -1,51 +1,44 @@
+#include <assert.h>
 #include <math.h>
 #include <gtk/gtk.h>
 
 #include "common.h"
 
-double *theta;
-
 /* ----------------------------------------------------------------------
 --
--- do_gtk_realise
+-- do_gtk_draw
 --
 ---------------------------------------------------------------------- */
-gboolean do_gtk_draw(GtkWidget *widget, GdkEventExpose *event, void *client_data)
+gboolean do_gtk_draw(GtkWidget *widget, cairo_t *cr, void *client_data)
 {
-  bio *v = (bio*)client_data;
+  bio_t *v = (bio_t*)client_data;
 
   // "convert" the G*t*kWidget to G*d*kWindow (no, it's not a GtkWindow!)
-  GdkWindow* window = gtk_widget_get_window(widget);  
+  GdkWindow* window = gtk_widget_get_window(widget);
+
+  guint width = gtk_widget_get_allocated_width (widget);
+  guint height = gtk_widget_get_allocated_height (widget);
+
+  //  fprintf(stdout, "draw(%d, %d)\n", width, height);
+  
+  v->width = width;
+  v->height = height;
 
   cairo_region_t * cairoRegion = cairo_region_create();
   GdkDrawingContext * drawingContext;
   drawingContext = gdk_window_begin_draw_frame(window, cairoRegion);
-  
-  //  cairo_t *cr = gdk_cairo_create(gtk_widget_get_window(win));
+  cairo_t * cr_win = gdk_drawing_context_get_cairo_context(drawingContext);
 
-  cairo_t * cr = gdk_drawing_context_get_cairo_context(drawingContext);
-  v->cairo(cr);
+  v->cairo(cr_win);
+  
   gdk_window_end_draw_frame(window, drawingContext);
   cairo_region_destroy(cairoRegion);
-  (*theta)--; 
+  v->theta -= 1.0;
   gtk_widget_queue_draw(widget);
 
   return 0;
 }
 
-/* ----------------------------------------------------------------------
---
--- update_theta
---
----------------------------------------------------------------------- */
-gboolean update_angle(gpointer data)
-{
-    GtkWidget *w = (GtkWidget*)data;
-    (*theta)++;
-    gtk_widget_queue_draw(w);
-    return 1;
-
-}
 
 /* ----------------------------------------------------------------------
 --
@@ -55,34 +48,39 @@ gboolean update_angle(gpointer data)
 
 int main(int argc, char *argv[])
 {
-  bio v(argc, argv);
+  bio_t bio(argc, argv);
   gtk_init (&argc, &argv);
-  GdkVisual *vis = gdk_visual_get_best_with_depth(32);
+
+  GdkScreen *screen = gdk_screen_get_default();
+  GdkVisual *visual = gdk_screen_get_rgba_visual(screen);
 
   GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_widget_set_visual(window, vis);
+  gtk_widget_set_visual(window, visual);
 
   GtkWidget *drawing = gtk_drawing_area_new();
   gtk_container_add(GTK_CONTAINER(window), drawing);
-
+  gtk_widget_set_name(window, "drawing");
+  
   g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  g_signal_connect(G_OBJECT(drawing), "draw", G_CALLBACK(do_gtk_draw), &bio);
 
-  //      g_signal_connect(G_OBJECT(t->window), "realize", G_CALLBACK(do_gtk_realise), t);
-  g_signal_connect(G_OBJECT(drawing), "draw", G_CALLBACK(do_gtk_draw), &v);
-  //      g_signal_connect(G_OBJECT(t->drawing), "realize", G_CALLBACK(do_gtk_realise), t);
-  //      g_signal_connect(G_OBJECT(t->drawing), "configure-event", G_CALLBACK(do_gtk_configure), t);
-
-  gtk_widget_set_size_request(GTK_WIDGET(drawing), v.width, v.height);
-
+  gtk_widget_set_size_request(GTK_WIDGET(drawing), bio.width, bio.height);
   gtk_window_set_decorated(GTK_WINDOW(window), 0);
 
-  GdkRGBA bg = {0.0, 0.0, 0.0, 0.0};
+  //  GdkRGBA bg = {0.0, 0.0, 0.0, 0.0};  
+  //  gtk_widget_override_background_color(window, (GtkStateFlags)0, &bg);
+  //  gtk_widget_override_background_color(drawing, (GtkStateFlags)0, &bg);
+
+  GtkCssProvider *provider = gtk_css_provider_new ();
+  assert(gtk_css_provider_load_from_path(provider, "bio.css", NULL));
+  // gtk_css_provider_load_from_data (provider, css, -1, NULL);
+  gtk_style_context_add_provider_for_screen (gdk_screen_get_default(),
+					     GTK_STYLE_PROVIDER(provider),
+					     GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
   
-  gtk_widget_override_background_color(window, (GtkStateFlags)0, &bg);
-  gtk_widget_override_background_color(drawing, (GtkStateFlags)0, &bg);
   gtk_widget_show_all(window);
 
-  theta = &v.rotation;
   //  g_timeout_add_seconds(1, update_angle, drawing);
 
   gtk_main();
