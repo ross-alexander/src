2677e3ff227e8785b293ebf98fcddb0bcc6058e8
Ross Alexander <evilross@yahoo.co.uk>
2025-02-27 17:24:25 +0000

    Convert to std::filesystem

--- tile/tile-scanner.cc
+++ tile/tile-scanner.cc
@@ -12,10 +12,12 @@
 #include <string>
 #include <ctime>
 #include <algorithm>
+#include <random>
+#include <filesystem>
 
 struct scanner_t {
-  std::vector<const char*> dir;
-  std::vector<std::string> file;
+  std::vector<std::filesystem::path> dir;
+  std::vector<std::filesystem::path> file;
   int index;
 };
 
@@ -156,7 +158,7 @@ int dirs_get(lua_State *L)
   
   for(auto const& value: scanner->dir)
     {  
-      lua_pushstring(L, value);
+      lua_pushstring(L, value.c_str());
       lua_rawseti(L, -2, index++);
     }
   return 1;
@@ -170,37 +172,24 @@ int dirs_get(lua_State *L)
 --
 ---------------------------------------------------------------------- */
 
-static void rescan_dir(scanner_t *s, const char *dirpath)
+static void rescan_dir(scanner_t *s, std::filesystem::path &dirpath)
 {
-  DIR *dirp;
-  unsigned flen = pathconf(dirpath, _PC_NAME_MAX) + 1;
-  unsigned dlen = offsetof(struct dirent, d_name) + flen;
-  char *path = (char*)calloc(flen, 1);
-  struct dirent **namelist;
-  struct stat statbuf;
-  int dcount;
-
-  if ((dcount = scandir(dirpath, &namelist, 0, alphasort)) > 0)
+  if (is_directory(dirpath))
     {
-      for (int i = 0; i < dcount; i++)
+      
+      for (auto const& dir_entry : std::filesystem::directory_iterator{dirpath})
 	{
-	  snprintf(path, flen, "%s/%s", dirpath, namelist[i]->d_name);
-	  
-	  if (stat(path, &statbuf) == 0)
+	  std::filesystem::path p = dir_entry.path();
+	  if (is_regular_file(p))
 	    {
-	      if (S_ISREG(statbuf.st_mode))
-		{
-		  s->file.push_back(std::string(path));
-		}
-	      if (S_ISDIR(statbuf.st_mode) && (strcmp(".", namelist[i]->d_name)) && (strcmp("..", namelist[i]->d_name)))
-		{
-		  rescan_dir(s, path);
-		}
+	      s->file.push_back(p);
 	    }
-	}
-      free(namelist);
+	  if (is_directory(p))
+	    {
+	      rescan_dir(s, p);
+	    }
+	}      
     }
-  free(path);
 } 
 
 
@@ -222,10 +211,12 @@ int rescan(lua_State *L)
   
   for (unsigned i = 0; i < scanner->dir.size(); i++)
     {
-      const char *dirpath = scanner->dir[i];
-      rescan_dir(scanner, dirpath);
+      rescan_dir(scanner, scanner->dir[i]);
     }
-  std::random_shuffle(scanner->file.begin(), scanner->file.end());
+  std::random_device rd;
+  std::mt19937 g(rd());
+  
+  std::shuffle(scanner->file.begin(), scanner->file.end(), g);
   scanner->index = 0;
   return 1;
 }
@@ -246,7 +237,7 @@ int scanner_t___tostring(lua_State *L)
   unsigned int len = 0;
   for (int i = 0; i < scanner->dir.size(); i++)
     {
-      len += strlen(scanner->dir[i]) + 2;
+      len += strlen(scanner->dir[i].c_str()) + 2;
     }
   char buf[1024 + len];
 
