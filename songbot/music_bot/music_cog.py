7c29fe4cec3bc8a6786e34363cf038788512c1f9
Ross Alexander <evilross@yahoo.co.uk>
2025-12-17 11:37:27 +0000

    Add play loop

--- songbot/music_bot/music_cog.py
+++ songbot/music_bot/music_cog.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 import argparse
 from discord.ext import commands
@@ -33,6 +34,42 @@ class music_cog(commands.Cog):
         else:
             return None
 
+    async def play_next(self):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+
+            #get the first url
+            path = self.music_queue[0][0]
+
+            #remove the first element as you are currently playing it
+            self.music_queue.pop(0)
+            self.vc.play(discord.FFmpegPCMAudio(path), after=lambda e: asyncio.run_coroutine_threadsafe(self.play_next(), self.bot.loop))
+        else:
+            self.is_playing = False
+            
+        
+    async def play_music(self, ctx):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+
+            path = self.music_queue[0][0]
+            if self.vc == None or not self.vc.is_connected():
+                self.vc = await self.music_queue[0][1].connect()
+
+                #in case we fail to connect
+                if self.vc == None:
+                    await ctx.send("```Could not connect to the voice channel```")
+                    return
+                else:
+                    await self.vc.move_to(self.music_queue[0][1])
+            #remove the first element as you are currently playing it
+            self.music_queue.pop(0)
+            self.vc.play(discord.FFmpegPCMAudio(path), after=lambda e: asyncio.run_coroutine_threadsafe(self.play_next(), self.bot.loop))
+        else:
+            self.is_playing = False
+#            self.vc.play(discord.FFmpegPCMAudio(path))
+
+
     @commands.command(name="play", aliases=["p","playing"], help="Plays a selected song from youtube")
     async def play(self, ctx, *args):
         query = " ".join(args)
@@ -49,11 +86,9 @@ class music_cog(commands.Cog):
                 await ctx.send("```Could not download the song. Incorrect format try another keyword. This could be due to playlist or a livestream format.```")
             else:
                 await ctx.send(f"Found {song}")
-                self.vc = await voice_channel.connect()
-                if self.vc == None:
-                    await ctx.send("```Could not connect to the voice channel```")
-                    return
-                self.vc.play(discord.FFmpegPCMAudio(song))
+                self.music_queue.append([song, voice_channel])
+                if self.is_playing == False:
+                    await self.play_music(ctx)
 
 
     @commands.command(name="queue", aliases=["q"], help="Displays the current songs in queue")
@@ -62,6 +97,9 @@ class music_cog(commands.Cog):
         for i in range(0, len(self.music_queue)):
             retval += f"#{i+1} -" + self.music_queue[i][0]['title'] + "\n"
 
+        if self.is_playing:
+            await ctx.send("Currently playing")
+        
         if retval != "":
             await ctx.send(f"```queue:\n{retval}```")
         else:
