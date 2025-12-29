89613e78f0114c83e50296f48bf460afff9f2d38
Ross Alexander <evilross@yahoo.co.uk>
2025-12-29 13:06:06 +0000

    Add size

--- thumbnail/thumb.lua
+++ thumbnail/thumb.lua
@@ -8,23 +8,26 @@ function compose(index, tn, cols, size, border)
    tn:load_images()
    local images = tn.image_table
    local rows = ((#images-1)//cols)+1
-   local fontsize = 16
-
+   local fontsize = 12
+   local family = "Linux Biolinum O"
+   
    local width = (size + border) * cols + border
    local height = (size + border + fontsize) * rows + border
    local dst = image_t.new(width, height)
    dst:fill("#FFFFFF")
-   
+
+   local bg = image_t.new(size, size)
+   bg:fill("#000000")
+
    local count = 0
    for path, image in pairs(images) do
       print(string.format("%s [%f × %f]", path, image.width, image.height))
-      local x = border + (border+size)*(count%cols)
-      local y = border + (border+size+fontsize)*(count//cols)
+      local x = border + (border + size) * (count % cols)
+      local y = border + (border + size + fontsize) * (count // cols)
       local scale = image:scale_to(size)
-      local bg = image_t.new(size, size)
-
-      bg:fill("#000000")
+      
       dst:compose(bg, x, y)
+
 --      dst:compose(scale, x, y)
       local cx = x + (size - scale.width)/2
       local cy = y + (size - scale.height)/2
@@ -32,12 +35,18 @@ function compose(index, tn, cols, size, border)
       dst:frame(x, y, size, size, 2.0, "#FF0000")
       local name = image:name()
       if (not (name == nil)) then
-	 local bb = dst:add_text(x, y + size, size, fontsize, "Linux Libertine O", name)
+	 local bb = dst:add_text(x, y + size, size, fontsize, family, name)
       end
       count = count + 1
    end
    dst:save(string.format("202507-%02d.jpg", index))
 end   
+
+-- ----------------------------------------------------------------------
+--
+-- main
+--
+-- ----------------------------------------------------------------------
 
 local tn = thumbnail_t.new()
 tn:dir_add("/locker/images/202507/")
@@ -48,11 +57,12 @@ tn:dir_scan()
 -- end
 
 local rows = 4
-local cols = 10
+local cols = 9
+local size = 200
 
 -- split into integer indexed table of thumbnails
 
 local split = tn:split(rows * cols)
 for i, v in ipairs(split) do
-   compose(i, v, cols, 200, 5)
+   compose(i, v, cols, size, 5)
 end
