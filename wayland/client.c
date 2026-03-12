a856c4b8932fa6b35646bee991752b53579a1bb1
Ross Alexander <evilross@yahoo.co.uk>
2026-03-12 10:25:46 +0000

    Save and push

--- wayland/client.c
+++ wayland/client.c
@@ -22,8 +22,8 @@
 #include "xdg-shell-client-protocol.h"
 
 #include <cairo.h>
-
-#include <gegl.h>
+#include <glycin.h>
+#include <babl/babl.h>
 
 #include <lua.h>
 #include <lualib.h>
@@ -546,33 +546,109 @@ int tile_surface_t_configuration(lua_State *L)
    --
    ---------------------------------------------------------------------- */
 
-int tile_surface_t_set_source_file_signal(GeglNode *node, GeglRectangle *rect, void *context)
-{
-  printf("Callback\n");
-  return 0;
-}
-
 int tile_surface_t_set_source_file(lua_State *L)
 {
+  GError *error;
   struct tile_surface_t** handle = (struct tile_surface_t**)luaL_checkudata(L, 1, "tile_surface_t");
   const char *path = luaL_checkstring(L, 2);
-  GeglNode *load;
-  GeglRectangle size;
-  //  cairo_surface_t *image;
 
   printf("tile_surface_t_set_source_file(%s)\n", path);
-  
-  load = gegl_node_new();
 
-  g_signal_connect (load, "invalidated", G_CALLBACK (tile_surface_t_set_source_file_signal), 0);
-  
-  gegl_node_set(load,
-		"operation", "gegl:load",
-		"path", path,
-		NULL);
+  GFile *file = g_file_new_for_path(path);
 
-  size = gegl_node_get_bounding_box(load);
-  printf("%5d %5d %5d %5d\n", size.x, size.y, size.width, size.height);
+  GlyLoader *loader = gly_loader_new(file);
+
+  /* --------------------
+     -- Load image and return 0 if fails
+     -------------------- */
+  
+  GlyImage *image = gly_loader_load(loader, &error);
+  if (image == nullptr)
+    {
+      fprintf(stderr, "Failed to load %s: %s\n", path, error->message);
+      return 0;
+    }
+  
+  printf("Image %s loaded\n", path);
+
+  /* --------------------
+     Get first and probably only frame, exit if fails
+     -------------------- */
+  
+  GlyFrame *frame = gly_image_next_frame(image, &error);
+
+  if (frame == nullptr)
+    {
+      fprintf(stderr, "Failed to load frame: %s\n", error->message);
+      g_object_unref(image);
+      g_object_unref(loader);
+      return 0;
+    }
+
+  /* --------------------
+     Get format and create Babl
+     -------------------- */
+  
+  GlyMemoryFormat format = gly_frame_get_memory_format(frame);
+  char *description = nullptr;
+  const Babl *babl_format_src = nullptr;
+
+  switch(format)
+    {
+    case GLY_MEMORY_R8G8B8:
+      babl_format_src = babl_format_with_space("R'G'B' u8", NULL);
+      description = "8-bit RGB";
+      break;
+    default:
+      babl_format_src = nullptr;
+      description = "Unsupported format";
+      break;
+    }
+  uint32_t height = gly_frame_get_height(frame);
+  uint32_t width = gly_frame_get_width(frame);
+  uint32_t stride_src = gly_frame_get_stride(frame);
+  gboolean alpha = gly_memory_format_has_alpha(format);
+
+  if (babl_format_src)
+    {
+      GBytes *src = gly_frame_get_buf_bytes(frame);
+      gsize buffer_src_size;
+      gconstpointer buffer_src = g_bytes_get_data(src, &buffer_src_size);
+      printf("Size should be %d, has %ld\n", height * stride_src, buffer_src_size);
+      
+      cairo_format_t cairo_format = alpha ? CAIRO_FORMAT_ARGB32 : CAIRO_FORMAT_RGB24;
+
+      uint32_t stride_dst = cairo_format_stride_for_width(cairo_format, width);
+
+      void *buffer_dst = malloc(stride_dst * height);
+      
+      cairo_surface_t* surface = cairo_image_surface_create_for_data(buffer_dst, cairo_format, width, height, stride_dst);
+      const Babl *babl_format_dst = alpha ? babl_format("cairo-ARGB32") : babl_format("cairo-RGB24");
+
+      printf("Converting from %s [%s] format %s [%d] to %s [%d]\n", description, alpha ? "alpha" : "no alpha",
+	     babl_get_name(babl_format_src),
+	     babl_format_get_bytes_per_pixel(babl_format_src),
+	     babl_get_name(babl_format_dst),
+	     babl_format_get_bytes_per_pixel(babl_format_dst)
+	     );
+
+      printf("Image width [%d] height [%d]\n", width, height);
+      
+      printf("Source stride [%d] Destination stride [%d]\n", stride_src, stride_dst);
+      
+      babl_process_rows(babl_fish(babl_format_src, babl_format_dst),
+			buffer_src,
+			stride_src,
+			buffer_dst,
+			stride_dst,
+			width,
+			height);
+      cairo_set_source_surface((*handle)->cairo, surface, 0.0, 0.0);
+    }
+  g_object_unref(frame);
+  g_object_unref(image);
+  g_object_unref(loader);
+  
   return 0;
 }
 
@@ -628,7 +704,7 @@ void tile_surface_init(lua_State *L)
 int main(int argc, char *argv[])
 {
   struct client_state state = { 0 };
-  gegl_init(&argc, &argv);
+  babl_init();
 
   /* lua */
   state.luastate = luaL_newstate();
@@ -640,7 +716,6 @@ int main(int argc, char *argv[])
       fprintf(stderr, "%s\n", lua_tostring(state.luastate, -1));
       exit(1);
     }
-
   
   /* wayland display */
   state.wl_display = wl_display_connect(NULL);
