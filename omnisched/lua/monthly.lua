d71afa7fedbbdf13cdf6ecc22db50e748aae990d
Ross Alexander <evilross@yahoo.co.uk>
2026-03-24 08:36:18 +0000

    Monthly removed from class method

--- omnisched/lua/monthly.lua
+++ omnisched/lua/monthly.lua
@@ -1,15 +1,26 @@
+-- ----------------------------------------------------------------------
+--
+-- Old example where the monthly backups were the weekend of the
+-- second Saturday
+--
+-- ----------------------------------------------------------------------
+
 require("date")
+
+function monthly(d)
+   return(d:strftime("\t-day %d -month %b"))
+end
 
 local d = Date.new("jan 1")
 
 print("*** Saturday schedule for " .. d:strftime("%Y") .. " ***")
-local i
+
 for i = 1,12
 do
-	print(Date.new("second saturday of month "..i.." at 20:00"):monthly())
+   print(monthly(Date.new("second saturday of month "..i.." at 20:00")))
 end
 print "*** Sunday schedule ***"
 for i = 1,12
 do
-	print((Date.new("second saturday of month "..i.." at 20:00") + 1):monthly())
+   print(monthly(Date.new("second saturday of month "..i.." at 20:00") + 1))
 end
